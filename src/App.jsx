a5e4db7cd8c518a66119b84231bd3c8ffbfae968
devrathul <rathulp2@gmail.com>
2026-09-20 23:46:10 +0530

    routing implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
-function App() {
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import User from './components/User'
+import NotFound from './components/NotFound'
+import Root from './Root'
+
+
+const router = createBrowserRouter(
+  [{
+    path: "/",
+    element: <Root />,
+    children: [{
+      path: "/",
+      element: <Home />,
+    }, {
+      path: "user",
+      element: <User />,
+    }, {
+      path: "about",
+      element: <About />,
+    }, {
+      path: "*",
+      element: <NotFound />,
+    }]
+  }]
+)
+
+const App = () => {
 
   return (
     <>
-      <h1>Hi</h1>
+      <RouterProvider router={router} />
     </>
   )
 }
